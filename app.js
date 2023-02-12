c4b6380cb965f8857fb0f98d27a80547da330e3b
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2023-02-12 20:28:18 +0700

    feat: add website portfolio command

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ const tmi = require('tmi.js')
 
 const regexpCommand = new RegExp(/^!([a-zA-Z0-9]+)(?:\W+)?(.*)?/)
 
-// const commands = {
-//   upvote: {
-//     response: (argument) => `Successfully upvoted ${argument}`,
-//   },
-//   id: {
-//     response: 'Valorant: KazeCrowz#1217',
-//   },
-// }
-
 const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -27,31 +18,7 @@ const client = new tmi.Client({
 
 client.connect()
 
-// client.on('message', async (channel, context, message) => {
-//   const isNotBot =
-//     context.username.toLowerCase() !==
-//     process.env.TWITCH_BOT_USERNAME.toLowerCase()
-//
-//   if (!isNotBot) return
-//
-//   const [raw, command, argument] = message.match(regexpCommand)
-//
-//   const { response } = commands[command] || {}
-//
-//   if (typeof response === 'function') {
-//     client.say(channel, response(argument))
-//   } else if (typeof response === 'string') {
-//     client.say(channel, response)
-//   }
-//   console.log(`${context['display-name']}: ${message}`)
-// })
-
 client.on('message', (channel, tags, message, self) => {
-  // if (self) return
-  //
-  // if (message.toLowerCase() === '!id') {
-  //   client.say(channel, 'valorant: KazeCrowz#1217')
-  // }
   if (self || !message.startsWith('!')) return
 
   const args = message.slice(1).split(' ')
@@ -70,5 +37,9 @@ client.on('message', (channel, tags, message, self) => {
       channel,
       'kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid kaze TombRaid'
     )
+  }
+
+  if (command === 'website') {
+    client.say(channel, 'kazecrowz.dev')
   }
 })
